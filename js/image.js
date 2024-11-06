e0c77a46e0df5df52dd218de39eee00fdb5f2e26
lk34567 <laibah.khan@student.shu.ac.uk>
2024-11-06 18:35:15 +0000

    edit27

--- js/image.js
+++ js/image.js
@@ -1,2 +1,20 @@
-
+let imageAr = [
+    'images/ban1.jpg',
+    'images/ban2.jpg',
+    'images/ban3.jpg',
+    'images/ban4.jpg',
+    'images/ban5.jpg',
+    'images/ban6.jpg'
+  ];
+  
+  let currentCounter = 0;
+  setInterval(chgImage, 4000);
+  
+  function chgImage() {
+    currentCounter++;
+    if (currentCounter >= imageAr.length) {
+      currentCounter = 0;
+    }
+    document.getElementById('Images').setAttribute('src', imageAr[currentCounter]);
+  }
   
