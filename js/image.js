8f3a763a092f361321550aac66afd090172749c3
lk34567 <laibah.khan@student.shu.ac.uk>
2024-11-06 12:43:45 +0000

    edit22

--- js/image.js
+++ js/image.js
@@ -6,3 +6,15 @@ let imageAr = [
  'images/banner5.png',
 'images/banner6.png'
 ]
+
+let currentIndex = 0;
+
+function chgImage () {
+    setInterval(chgImage,200)
+    currentIndex++;
+    if (currentIndex >= imageAr.length){
+        currentIndex = 0;
+       
+    }
+    document.getElementById('images').setAttribute('src', imageAr[cuurentindex]);
+}  
