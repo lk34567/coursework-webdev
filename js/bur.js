c1c77bc4049c687ef1c28a5f56994ebca0ae942c
lk34567 <laibah.khan@student.shu.ac.uk>
2024-11-20 18:11:49 +0000

    edit50

--- js/bur.js
+++
@@ -1,9 +0,0 @@
-(()=>{
-    const burgerbutton = document.getElementById('bugeric');
-    const navbar = document.querySelector('nav');
-
-    burgerbutton.addEventListener("click",() => { 
-        navbar.classList.toggle("open");
-    });
-
-})();
