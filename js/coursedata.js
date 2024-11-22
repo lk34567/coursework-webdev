dcf09c8dc539fe4720f0b99ffdbbb88697ec6228
lk34567 <laibah.khan@student.shu.ac.uk>
2024-11-22 18:50:46 +0000

    edit52

--- js/coursedata.js
+++ js/coursedata.js
@@ -1,38 +1,38 @@
 (() => {
     console.info("ready");
-    const courselist = document.getElementById("courselist");
-    fetch("data/course.json")
+    const courselist = document.getElementById("compcourselist");
+    fetch("data/compcourse.json")
     .then(response => response.json())
     .then(data => {
-        console.dir(courselist);
-        data["courselist"].foreach((item) => {
+        console.dir(data);
+        data["compcourselist"].forEach((item) => {
           const row = document.createElement("tr");
-          const CourseTitlecell = document.createElement("tr");
-          const CourseTypecell = document.createElement("tr");
-          const Coursesummarycell = document.createElement("tr");
-          const CourseAwardNamecell  = document.createElement("tr");
-          const UcasCodecell  = document.createElement("tr");
-          const UcasPointscell  = document.createElement("tr");
-          const YearOfEntrycell = document.createElement("tr");
-          const ModeOfAttendancecell = document.createElement("tr");
-          const StudyLengthcell = document.createElement("tr");
-          const hasFoundationYearcell = document.createElement("tr");
-          const courseSubjectcell = document.createElement("tr");
-          const NoLongerRecruitingcell  = document.createElement("tr");
+          const CourseTitlecell = document.createElement("td");
+          const CourseTypecell = document.createElement("td");
+          const Coursesummarycell = document.createElement("td");
+          const CourseAwardNamecell  = document.createElement("td");
+          const UcasCodecell  = document.createElement("td");
+          const UcasPointscell  = document.createElement("td");
+          const YearOfEntrycell = document.createElement("td");
+          const ModeOfAttendancecell = document.createElement("td");
+          const StudyLengthcell = document.createElement("td");
+          const hasFoundationYearcell = document.createElement("td");
+          const courseSubjectcell = document.createElement("td");
+          const NoLongerRecruitingcell  = document.createElement("td");
         
         
           CourseTitlecell.textContent = item.CourseTitle
           CourseTypecell.textContent = item.CourseType
-          Coursesummarycell.textContent = item.Coursesummary
+          Coursesummarycell.textContent = item.CourseSummary
           CourseAwardNamecell.textContent =  item.CourseAwardName
           UcasCodecell.textContent = item.UcasCode
           UcasPointscell.textContent = item.UcasPoints
           YearOfEntrycell.textContent = item.YearOfEntry
           ModeOfAttendancecell.textContent = item.ModeOfAttendance
           StudyLengthcell.textContent = item.StudyLength
-          hasFoundationYearcell.textContent = item.hasFoundationYear
-          courseSubjectcell.textContent = item.courseSubject
-          NoLongerRecruitingcell.textContent = item.NoLongerRecruiting
+          hasFoundationYearcell.textContent = item.HasFoundationYear 
+          courseSubjectcell.textContent = item.CourseSubject
+          NoLongerRecruitingcell.textContent = item.NoLongerRecruiting 
 
           row.appendChild(CourseTitlecell);
           row.appendChild(CourseTypecell);
@@ -62,4 +62,72 @@
   
    
 })();
+
+
+
+(() => {
+    console.info("ready");
+    const courselist = document.getElementById("descourselist");
+    fetch("data/descourse.json")
+    .then(response => response.json())
+    .then(data => {
+        console.dir(data);
+        data["descourselist"].forEach((item) => {
+          const row = document.createElement("tr");
+          const CourseTitlecell = document.createElement("td");
+          const CourseTypecell = document.createElement("td");
+          const Coursesummarycell = document.createElement("td");
+          const CourseAwardNamecell  = document.createElement("td");
+          const UcasCodecell  = document.createElement("td");
+          const UcasPointscell  = document.createElement("td");
+          const YearOfEntrycell = document.createElement("td");
+          const ModeOfAttendancecell = document.createElement("td");
+          const StudyLengthcell = document.createElement("td");
+          const hasFoundationYearcell = document.createElement("td");
+          const courseSubjectcell = document.createElement("td");
+          const NoLongerRecruitingcell  = document.createElement("td");
+        
+        
+          CourseTitlecell.textContent = item.CourseTitle
+          CourseTypecell.textContent = item.CourseType
+          Coursesummarycell.textContent = item.CourseSummary
+          CourseAwardNamecell.textContent =  item.CourseAwardName
+          UcasCodecell.textContent = item.UcasCode
+          UcasPointscell.textContent = item.UcasPoints
+          YearOfEntrycell.textContent = item.YearOfEntry
+          ModeOfAttendancecell.textContent = item.ModeOfAttendance
+          StudyLengthcell.textContent = item.StudyLength
+          hasFoundationYearcell.textContent = item.HasFoundationYear 
+          courseSubjectcell.textContent = item.CourseSubject
+          NoLongerRecruitingcell.textContent = item.NoLongerRecruiting 
+
+          row.appendChild(CourseTitlecell);
+          row.appendChild(CourseTypecell);
+          row.appendChild(Coursesummarycell);
+          row.appendChild(CourseAwardNamecell);
+          row.appendChild(UcasCodecell);
+          row.appendChild(UcasPointscell);
+          row.appendChild(YearOfEntrycell);
+          row.appendChild(ModeOfAttendancecell);
+          row.appendChild(StudyLengthcell);
+          row.appendChild(hasFoundationYearcell);
+          row.appendChild(courseSubjectcell);
+          row.appendChild(NoLongerRecruitingcell);
+          courselist.appendChild(row);
+    });
+})
+
+        .catch((error)=> {
+            console.error("error loading JSON:" ,error);
+
+        });
+
+
+
+
+
+  
+   
+})();
+
 
